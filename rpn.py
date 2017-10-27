7985053cfa52bc0054eacd9b0b1b37db67579682
ayangz <ayangz@umich.edu>
2017-10-27 12:45:47 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import operator
+
+ops = {
+    '+': operator.add,
+    '-': operator.sub
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            stack.append(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            stack.append(function(arg1, arg2))
+    # print(stack)
+    return stack.pop()
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
